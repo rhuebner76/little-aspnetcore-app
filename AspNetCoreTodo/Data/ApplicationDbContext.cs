4f761a133580cbf932b947c794679476323316db
russell huebner <russell.j.huebner@outlook.com>
2024-09-02 12:22:29 -0700

    added persistance and add | complete item feature

--- AspNetCoreTodo/Data/ApplicationDbContext.cs
+++ AspNetCoreTodo/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using AspNetCoreTodo.Models;
 
 namespace AspNetCoreTodo.Data;
 
@@ -9,4 +10,6 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
     }
+
+    public DbSet<TodoItem> Items { get; set; }
 }
--- AspNetCoreTodo/Services/ITodoItemService.cs
+++ AspNetCoreTodo/Services/ITodoItemService.cs
@@ -5,4 +5,6 @@ namespace AspNetCoreTodo.Services;
 public interface ITodoItemService
 {
     Task<TodoItem[]> GetIncompleteItemsAsync();
+    Task<bool> AddItemAsync(TodoItem newItem);
+    Task<bool> MarkDoneAsync(Guid id);
 }
